3bd28b4d54f70b5fb18741886fff698de48ec7b7
GarboMuffin <tommyweber33@gmail.com>
2019-01-13 01:29:40 -0600

    Fix Scratch 2 project loading
    Turns out I forgot that .s2b's use file IDs instead of hashes.
    Disabled sb2 asset deduping because files having the same md5 is normal.
    
    Reasons like this are why the Scratch 2 and 3 loaders don't directly
    share any code: There's just too many differences.

--- loader.js
+++ loader.js
@@ -33,7 +33,7 @@ window.loader = (function() {
       return fetch(ASSETS_API.replace('$path', path))
         .then((request) => request.arrayBuffer())
         .then((buffer) => {
-          result.files.push({path: data.md5ext, data: buffer})
+          result.files.push({path: data.md5ext, data: buffer});
           progressHooks.finishTask();
         });
     }
@@ -63,6 +63,10 @@ window.loader = (function() {
       .then((projectData) => {
         result.files.push({path: 'project.json', data: JSON.stringify(projectData)});
 
+        if (!Array.isArray(projectData.targets)) {
+          throw new Error('invalid sb3 project (missing targets)');
+        }
+
         const targets = projectData.targets;
         const costumes = [].concat.apply([], targets.map((t) => t.costumes || []));
         const sounds = [].concat.apply([], targets.map((t) => t.sounds || []));
@@ -81,35 +85,67 @@ window.loader = (function() {
     const PROJECTS_API = 'https://projects.scratch.mit.edu/internalapi/project/$id/get/';
     const ASSETS_API = 'https://cdn.assets.scratch.mit.edu/internalapi/asset/$path/get/';
 
+    const IMAGE_EXTENSIONS = [
+      'svg', 'png',
+    ];
+    const SOUND_EXTENSIONS = [
+      'wav',
+    ];
+
     const result = {
       title: id.toString(),
       extension: 'sb2',
       files: [],
     };
 
-    function addFile(data) {
-      progressHooks.newTask();
-      const path = data.md5 || data.baseLayerMD5;
-      return fetch(ASSETS_API.replace('$path', path))
-        .then((request) => request.arrayBuffer())
-        .then((buffer) => {
-          result.files.push({path: path, data: buffer})
-          progressHooks.finishTask();
-        });
+    // sb2 files have two ways of storing references to files.
+    // In the online editor they use md5 hashes which point to an API destination.
+    // In the offline editor they use separate accumlative file IDs for images and sounds.
+    // The files served from the Scratch API don't contain the file IDs we need to export a valid .sb2, so we must create those ourselves.
+
+    let soundAccumulator = 0;
+    let imageAccumulator = 0;
+    let projectData = null;
+
+    // Gets the md5 and extension of an object.
+    function md5Of(thing) {
+      return thing.md5 || thing.baseLayerMD5 || thing.penLayerMD5 || thing.toString();
     }
 
-    function dedupeAssets(assets) {
-      const result = [];
-      const seen = new Set();
-      for (const i of assets) {
-        const id = i.md5 || i.baseLayerMD5;
-        if (seen.has(id)) {
-          continue;
-        }
-        seen.add(id);
-        result.push(i);
+    function claimAccumlatedID(extension) {
+      if (IMAGE_EXTENSIONS.includes(extension)) {
+        return imageAccumulator++;
+      } else if (SOUND_EXTENSIONS.includes(extension)) {
+        return soundAccumulator++;
+      } else {
+        throw new Error('unknown extension: ' + extension);
       }
-      return result;
+    }
+
+    function addFile(data) {
+      progressHooks.newTask();
+
+      const md5 = md5Of(data);
+      const extension = md5.split('.').pop();
+      const accumlator = claimAccumlatedID(extension);
+      const path = accumlator + '.' + extension;
+
+      if ('baseLayerID' in data) {
+        data.baseLayerID = accumlator;
+      }
+      if ('soundID' in data) {
+        data.soundID = accumlator;
+      }
+      if ('penLayerID' in data) {
+        data.penLayerID = accumlator;
+      }
+
+      return fetch(ASSETS_API.replace('$path', md5))
+        .then((request) => request.arrayBuffer())
+        .then((buffer) => {
+          result.files.push({path: path, data: buffer});
+          progressHooks.finishTask();
+        });
     }
 
     progressHooks.start();
@@ -117,17 +153,18 @@ window.loader = (function() {
 
     return fetch(PROJECTS_API.replace('$id', id))
       .then((request) => request.json())
-      .then((projectData) => {
-        result.files.push({path: 'project.json', data: JSON.stringify(projectData)});
-
+      .then((pd) => {
+        projectData = pd;
         const children = projectData.children.filter((c) => !c.listName && !c.target);
-        const costumes = [].concat.apply([], children.map((c) => c.costumes || []));
-        const sounds = [].concat.apply([], children.map((c) => c.sounds || []));
-        const assets = dedupeAssets([].concat.apply([], [costumes, sounds]));
-
+        const targets = [].concat.apply([], [projectData, children]);
+        const costumes = [].concat.apply([], targets.map((c) => c.costumes || []));
+        const sounds = [].concat.apply([], targets.map((c) => c.sounds || []));
+        const assets = [].concat.apply([], [costumes, sounds, projectData]);
         return Promise.all(assets.map((a) => addFile(a)));
       })
       .then(() => {
+        // We must add the project JSON at the end because it may have been changed during the loading.
+        result.files.push({path: 'project.json', data: JSON.stringify(scratchProjectData)});
         progressHooks.finishTask();
         return result;
       });
@@ -167,5 +204,5 @@ window.loader = (function() {
     loadProject: loadProject,
     createArchive: createArchive,
     progressHooks: progressHooks,
-  }
+  };
 }());
