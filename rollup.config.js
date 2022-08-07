efc468e7ee74a985b0f286bc07469d37b1587ef9
GarboMuffin <muffin@mailbox.org>
2022-08-07 12:18:34 -0500

    Finish bundling

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,29 @@
-export default {
-  input: 'src/export.js',
-  output: {
-    file: 'lib/bundle.js',
-    format: 'umd',
-    name: 'SBDL',
-    globals: {
-      jszip: 'JSZip',
-      'cross-fetch': 'fetch'
-    }
+import commonjs from '@rollup/plugin-commonjs';
+import {nodeResolve} from '@rollup/plugin-node-resolve';
+
+export default [
+  {
+    // For npm
+    input: 'src/export.js',
+    output: {
+      file: 'lib/bundle.js',
+      format: 'cjs'
+    },
+    external: ['jszip', 'cross-fetch']
   },
-  external: ['jszip', 'cross-fetch']
-};
+  {
+    // For use in a <script>
+    input: 'src/export.js',
+    output: {
+      file: 'lib/bundle-standalone.js',
+      format: 'umd',
+      name: 'SBDL'
+    },
+    plugins: [
+      commonjs(),
+      nodeResolve({
+        browser: true
+      })
+    ]
+  }
+];
--- src/export.js
+++ src/export.js
@@ -1,7 +1,1 @@
-// Check dependencies in web bundle
-import JSZip from 'jszip';
-if (!JSZip) {
-  throw new Error('.sb downloader requires JSZip 3.x, which is missing. Please see the README.');
-}
-
 export * from './downloader';
