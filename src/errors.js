8ba4d585f4071a3a63537c2b8fcf696248fe4753
GarboMuffin <muffin@mailbox.org>
2022-08-08 00:52:39 -0500

    Redact project token from HTTPError message

--- src/errors.js
+++ src/errors.js
@@ -1,10 +1,12 @@
+const sanitizeURL = (url) => url.replace(/\?token=[^&#]+/, '?token=x');
+
 export class HTTPError extends Error {
   /**
    * @param {string} url
    * @param {number} status HTTP status
    */
   constructor (url, status) {
-    super(`Unexpected error ${status} while fetching ${url}`);
+    super(`Unexpected status ${status} while fetching ${sanitizeURL(url)}`);
     this.name = 'HTTPError';
     this.url = url;
     this.status = status;
