bdd82f56b6d69cd0bea647f83ab346ebe9f4c17d
GarboMuffin <muffin@mailbox.org>
2022-08-09 00:51:12 -0500

    Use ./ as vite base

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import legacy from '@vitejs/plugin-legacy';
 
 export default {
+  // Our website is just a single page. Use relative base path so that it can work from any folder
+  // without configuration.
+  base: './',
   plugins: [
     legacy({
       targets: [
