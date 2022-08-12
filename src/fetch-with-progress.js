add0a2a7351d885c200875c6ca7c1105889e9441
GarboMuffin <muffin@mailbox.org>
2022-08-12 13:48:24 -0500

    Implement project data download progress for Node.js
    Closes https://github.com/forkphorus/sb-downloader/issues/3

--- src/fetch-with-progress.js
+++ src/fetch-with-progress.js
@@ -7,7 +7,7 @@ import {AbortError, HTTPError} from './errors.js';
  * @param {AbortSignal} [abortSignal] 
  * @returns {Promise<ArrayBuffer>}
  */
-const fetchAsArrayBufferWithProgress = (url, progressCallback, abortSignal) => {
+const fetchAsArrayBufferWithProgress = async (url, progressCallback, abortSignal) => {
   // We can't always track real progress, but we should still fire explicit 0% and 100% complete events.
   progressCallback(0);
 
@@ -45,13 +45,31 @@ const fetchAsArrayBufferWithProgress = (url, progressCallback, abortSignal) => {
     });
   }
 
-  // Running in Node.js. For now we just won't have progress monitoring.
-  return fetch(url)
-    .then((r) => r.arrayBuffer())
-    .then((buffer) => {
-      progressCallback(1);
-      return buffer;
+  // Running in Node.js
+  const response = await fetch(url);
+  if (response.status !== 200) {
+    throw new HTTPError(url, response.status);
+  }
+  const total = +response.headers.get('content-length');
+  if (total) {
+    let loaded = 0;
+    response.body.on('data', (chunk) => {
+      // Content-Length is the size of the compressed data (before decoding Content-Encoding) but
+      // the chunks we receive here will be the decompressed data.
+      // We can rely on the implementation detail of node-fetch using a pipeline as the response
+      // body if Content-Encoding is used and read its bytesWritten property instead of summing
+      // the length of the chunks.
+      if (typeof response.body.bytesWritten === 'number') {
+        progressCallback(response.body.bytesWritten / total);
+      } else {
+        loaded += chunk.length;
+        progressCallback(loaded / total);
+      }
     });
+  }
+  const buffer = await response.arrayBuffer();
+  progressCallback(1);
+  return buffer;
 };
 
 export default fetchAsArrayBufferWithProgress;
