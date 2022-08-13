4349a6ad955bf6a7d94c79c2bf4c30e3823de788
GarboMuffin <muffin@mailbox.org>
2022-08-13 15:13:12 -0500

    Increase timeout on date tests

--- test/date.test.js
+++ test/date.test.js
@@ -13,7 +13,7 @@ test('date defaults to a constant arbitrary time', async () => {
   expect(zip.files['592bae6f8bb9c8d88401b54ac431f7b6.svg'].date.toUTCString()).toBe('Fri, 31 Dec 2021 00:00:00 GMT');
   expect(zip.files['83a9787d4cb6f3b7632b4ddfebf74367.wav'].date.toUTCString()).toBe('Fri, 31 Dec 2021 00:00:00 GMT');
   expect(project).toMatchSnapshot();
-});
+}, 30000);
 
 test('date option', async () => {
   const date = new Date(1000030000000);
@@ -25,4 +25,4 @@ test('date option', async () => {
   expect(zip.files['592bae6f8bb9c8d88401b54ac431f7b6.svg'].date.toUTCString()).toBe(date.toUTCString());
   expect(zip.files['83a9787d4cb6f3b7632b4ddfebf74367.wav'].date.toUTCString()).toBe(date.toUTCString());
   expect(project).toMatchSnapshot();
-});
+}, 30000);
